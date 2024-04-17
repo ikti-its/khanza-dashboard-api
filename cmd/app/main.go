23c22a2e3c171ebb7aafd1cc11934f7cdb4ade32
Muhammad Fathurrahman <90760976+fathoor@users.noreply.github.com>
2024-04-17 21:43:09 +0700

    Initial commit

---
+++ cmd/app/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"github.com/fathoor/go-modular/internal/app/config"
+	"github.com/fathoor/go-modular/internal/app/provider"
+	"github.com/jmoiron/sqlx"
+	"log"
+)
+
+func main() {
+	var (
+		cfg       = config.NewConfig()
+		fiber     = config.NewFiber(cfg)
+		postgres  = config.NewPostgres(cfg)
+		validator = config.NewValidator()
+		bootstrap = provider.Provider{App: fiber, Config: cfg, DB: postgres, Validator: validator}
+	)
+
+	defer func(postgres *sqlx.DB) {
+		err := postgres.Close()
+		if err != nil {
+			log.Fatalf("Failed to close database connection: %v", err)
+		}
+	}(postgres)
+
+	bootstrap.Provide()
+
+	if err := fiber.Listen(fmt.Sprintf(":%d", cfg.GetInt("APP_PORT", 8080))); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
+}
